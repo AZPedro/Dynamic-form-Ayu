40c324c65a414e1b086b140406c3f8357d6aade5
ped1256 <azevedo@devgrid.co.uk>
2020-04-14 11:16:13 -0300

    Create project AyuGate

---
+++ AyuGate/AyuGate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AyuGate
+//
+//  Created by Pedro Azevedo on 14/04/20.
+//  Copyright © 2020 AyuGate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
